e368505e27ff8f6e7b4a5aefea10fbfadf73b5fd
griguliana <165513968+griguliana@users.noreply.github.com>
2024-04-27 00:19:37 +0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,13 @@
+# Григорьева Ульяна, 15-я когорта — Дипломный проект
+
+order_body = {
+        "firstName": "Джейсон",
+        "lastName": "Стэтхемович",
+        "address": "Манежная, 1",
+        "metroStation": 4,
+        "phone": "+7 656 566 65 65",
+        "rentTime": 5,
+        "deliveryDate": "2024-04-14",
+        "comment": "Привезите быстрее",
+        "color": ["BLACK"]
+    }
