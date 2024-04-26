c9ba7813184228e1aaf444434ab507ffbc63deb4
griguliana <165513968+griguliana@users.noreply.github.com>
2024-04-27 00:02:41 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,5 @@
+# Григорьева Ульяна, 15-я когорта — Дипломный проект
+
+URL_SERVICE = "https://6fa840ea-7245-48ba-9386-806b814df022.serverhub.praktikum-services.ru"
+CREATE_ORDERS = "/api/v1/orders"
+TRACK_ORDERS = "/api/v1/orders/track"
